1b858afba37aa155e2097587816999881a5309f6
Luiz Renato <luiz504@hotmail.com>
2020-06-13 10:44:45 -0300

    typing classes

--- backend/src/services/EmailService.ts
+++ backend/src/services/EmailService.ts
@@ -15,12 +15,17 @@ interface ISendMailDTO {
   message: IMailMessage;
 }
 
+interface IMailService {
+  sendMail(request: ISendMailDTO): void;
+  //apparently use void when a function doesn't return anything
+}
+
 /**
  * DTO
  * Data Transfer Object()
  */
 
-class EmailService {
+class EmailService implements IMailService {
   sendMail({ to, message }: ISendMailDTO) {
     console.log(`Email sent to ${to.name}: ${message.subject}`);
   }
